73dbc4f59512b00411d2bef4b88791889ad8127a
Pavel.Dimitrov <2heads.pd@gmail.com>
2025-05-08 22:34:38 +0300

    improve request/response DTO typings

--- motostore-api/src/module/users/controllers/users.controller.ts
+++ motostore-api/src/module/users/controllers/users.controller.ts
@@ -1,14 +1,21 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { UserRequestBodyDto } from '../dto/users.dto';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateUserRequestBodyDto, UserResponseBodyDto } from '../dto/users.dto';
 import { UsersService } from '../services/users.service';
 
 @Controller('users')
 export class UsersController {
     constructor(private readonly usersService: UsersService) { }
 
+    @Get()
+    async getAllUsers(): Promise<UserResponseBodyDto[]> {
+        return this.usersService.getAllUsers();
+    }
+
+
     @Post()
-    async createUser(@Body() data: UserRequestBodyDto) {
+    async createUser(@Body() data: CreateUserRequestBodyDto): Promise<UserResponseBodyDto> {
         return this.usersService.createUser(data);
     }
+
 }
 

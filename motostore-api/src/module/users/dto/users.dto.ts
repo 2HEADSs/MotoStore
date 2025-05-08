78e66a0fc51e3a0f7a5c8609162eb6cabbda9046
Pavel.Dimitrov <2heads.pd@gmail.com>
2025-05-08 22:33:31 +0300

    update user.dto with UserResponseBodyDto

--- motostore-api/src/module/users/dto/users.dto.ts
+++ motostore-api/src/module/users/dto/users.dto.ts
@@ -1,10 +1,15 @@
 import { IsEmail, IsNotEmpty } from "class-validator";
 
-export class UserRequestBodyDto {
-  @IsNotEmpty()
-  @IsEmail()
-  email: string;
+export class CreateUserRequestBodyDto {
+    @IsNotEmpty()
+    @IsEmail()
+    email: string;
 
-  @IsNotEmpty()
-  password: string;
+    @IsNotEmpty()
+    password: string;
+}
+
+export class UserResponseBodyDto {
+    id: string;
+    email: string;
 }

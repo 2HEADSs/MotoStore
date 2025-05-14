575109f4c82e07dd82b52625706da3e5e75d1f59
Pavel.Dimitrov <2heads.pd@gmail.com>
2025-05-14 22:50:27 +0300

    implement "get profile" using JwtStrategy

--- motostore-api/src/modules/users/controllers/users.controller.ts
+++ motostore-api/src/modules/users/controllers/users.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
-import { CreateUserRequestBodyDto} from '../dto/users.dto';
+import { Body, Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
+import { CreateUserRequestBodyDto } from '../dto/users.dto';
 import { UsersService } from '../services/users.service';
 import { User } from 'src/common/interfaces/user.interface';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('users')
 export class UsersController {
@@ -16,6 +17,15 @@ export class UsersController {
     @Post()
     async createUser(@Body() data: CreateUserRequestBodyDto): Promise<User> {
         return this.usersService.createUser(data);
+    }
+
+    @UseGuards(AuthGuard('jwt'))
+    @Get('profile')
+    async getProfile(@Request() req): Promise<User | null> {
+        const user = await this.usersService.findUserByEmail(req.user.email);
+        if (!user) return null;
+        const { hashedPassword, ...userWithoutPassword } = user;
+        return userWithoutPassword;
     }
 
 }

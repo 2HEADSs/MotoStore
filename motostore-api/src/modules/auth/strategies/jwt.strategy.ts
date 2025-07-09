eb9d136798069defe8c814326a230e7ad71e1fb4
Pavel.Dimitrov <2heads.pd@gmail.com>
2025-07-09 17:51:04 +0300

    Update JwtAuthGuard to block users witn unactive account

--- motostore-api/src/modules/auth/strategies/jwt.strategy.ts
+++ motostore-api/src/modules/auth/strategies/jwt.strategy.ts
@@ -1,10 +1,11 @@
 import { Strategy, ExtractJwt } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { UsersService } from 'src/modules/users/services/users.service';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor() {
+  constructor(private readonly usersService: UsersService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
@@ -12,9 +13,12 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     });
   }
 
-  //TODO: Remove any
-  async validate(payload: any) {
-    // console.log("Paylod:", payload);
+  async validate(payload: { id: string; email: string; role: string }) {
+    const user = await this.usersService.getUserById(payload.id);
+    if (!user || user.isBlocked) {
+      throw new ForbiddenException('Account is disabled');
+    }
+    console.log("Paylod:", payload);
     return {
       id: payload.id,
       email: payload.email,

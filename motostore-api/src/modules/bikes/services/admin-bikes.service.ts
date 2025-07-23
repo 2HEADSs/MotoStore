32227087402f804ef690fcf209f125feb139b2c1
Pavel.Dimitrov <2heads.pd@gmail.com>
2025-07-23 15:29:02 +0300

    add updateBike service for admin

--- motostore-api/src/modules/bikes/services/admin-bikes.service.ts
+++ motostore-api/src/modules/bikes/services/admin-bikes.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { DatabaseService } from 'src/modules/database/database.service';
 import { Bike } from '@prisma/client';
+import { AdminUpdateBikeDto } from '../dto/adminUpdateBike.dto';
 
 @Injectable()
 export class AdminBikesService {
@@ -12,6 +13,31 @@ export class AdminBikesService {
     } catch (error) {
       console.error('Error fetching bikes in admin service:', error);
       throw new InternalServerErrorException('Failed to fetch bikes');
+    }
+  }
+
+  async updateBike(bikeId: string, dto: AdminUpdateBikeDto): Promise<Bike> {
+    const { price, ...bikeData } = dto;
+
+    try {
+      return await this.db.$transaction(async (tx) => {
+        const updatedBike = await tx.bike.update({
+          where: { id: bikeId },
+          data: bikeData,
+        });
+        if (price !== undefined) {
+          await tx.prices.create({
+            data: {
+              bikeId,
+              price,
+            },
+          });
+        }
+        return updatedBike;
+      });
+    } catch (error) {
+      console.error('Error updating bike in admin service:', error);
+      throw new InternalServerErrorException('Failed to update bike');
     }
   }
 }

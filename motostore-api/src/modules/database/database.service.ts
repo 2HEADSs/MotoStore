79e372a3ec6d63ecbc397e7cb28b298100e5ca76
Pavel.Dimitrov <2heads.pd@gmail.com>
2025-07-24 17:21:09 +0300

    Create getBikeWithLatestPrice in DatabaseService

--- motostore-api/src/modules/database/database.service.ts
+++ motostore-api/src/modules/database/database.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import { Bike, PrismaClient } from '@prisma/client';
+import { Decimal } from '@prisma/client/runtime/library';
 
 @Injectable()
 export class DatabaseService
@@ -12,5 +13,36 @@ export class DatabaseService
 
   async onModuleDestroy() {
     await this.$disconnect();
+  }
+
+  /**
+   * TODO: tx?
+   * @param id  bikeID
+   * @param tx  optional PrismaClient
+   */
+  async getBikeWithLatestPrice(
+    id: string,
+    tx: PrismaClient = this,
+  ): Promise<(Bike & { price: { price: number; createdAt: Date }[] }) | null> {
+    const bike = await tx.bike.findUnique({
+      where: { id },
+      include: {
+        price: {
+          orderBy: { createdAt: 'desc' },
+          take: 1,
+          select: { price: true, createdAt: true },
+        },
+      },
+    });
+
+    if (!bike) return null;
+    console.log(bike + "database.services.ts");
+    return {
+      ...bike,
+      price: bike.price.map((p) => ({
+        ...p,
+        price: (p.price as Decimal).toNumber(),
+      })),
+    } as Bike & { price: { price: number; createdAt: Date }[] };
   }
 }

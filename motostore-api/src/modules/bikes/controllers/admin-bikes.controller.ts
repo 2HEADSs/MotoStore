573f9bf8b606dbe8006b51f3c59af9549f3a7f66
Pavel.Dimitrov <2heads.pd@gmail.com>
2025-08-05 20:55:22 +0300

    create getOneBike  controller & service for admins

--- motostore-api/src/modules/bikes/controllers/admin-bikes.controller.ts
+++ motostore-api/src/modules/bikes/controllers/admin-bikes.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Get, Param, Patch, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  NotFoundException,
+  Param,
+  Patch,
+  UseGuards,
+} from '@nestjs/common';
 import { AdminBikesService } from '../services/admin-bikes.service';
 import { JwtAuthGuard } from 'src/modules/auth/guards/jwt-auth.guard';
 import { AdminGuard } from 'src/modules/auth/guards/admin.guard';
@@ -22,6 +30,18 @@ export class AdminBikesController {
   @ApiOperation({ summary: 'Admin: update any bike listing' })
   @ApiBody({ type: AdminUpdateBikeDto })
   updateBike(@Param('id') bikeId: string, @Body() dto: AdminUpdateBikeDto) {
+    if (!bikeId) {
+      throw new NotFoundException('Bike not found');
+    }
     return this.adminBikesService.updateBike(bikeId, dto);
+  }
+
+  @Get(':id')
+  @ApiOperation({ summary: 'Admin: get any bike listing' })
+  getBike(@Param('id') bikeId: string) {
+    if (!bikeId) {
+      throw new NotFoundException('Bike not found');
+    }
+    return this.adminBikesService.getOneBike(bikeId);
   }
 }
--- motostore-api/src/modules/bikes/services/admin-bikes.service.ts
+++ motostore-api/src/modules/bikes/services/admin-bikes.service.ts
@@ -1,8 +1,13 @@
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
 import { DatabaseService } from 'src/modules/database/database.service';
 import { Bike } from '@prisma/client';
 import { AdminUpdateBikeDto } from '../dto/adminUpdateBike.dto';
 import { BikeRepository } from '../repositories/bike.repository';
+import { Decimal } from '@prisma/client/runtime/library';
 
 @Injectable()
 export class AdminBikesService {
@@ -46,5 +51,30 @@ export class AdminBikesService {
       console.error('Error updating bike in admin service:', error);
       throw new InternalServerErrorException('Failed to update bike');
     }
+  }
+
+  async getOneBike(
+    bikeId: string,
+  ): Promise<Bike & { latestPrice: number | null }> {
+    const bike = await this.db.bike.findUnique({
+      where: { id: bikeId },
+      include: {
+        price: {
+          orderBy: { createdAt: 'desc' },
+          take: 1,
+          select: { price: true },
+        },
+      },
+    });
+    if (!bike) {
+      throw new NotFoundException('Bike not found');
+    }
+    return {
+      ...bike,
+      latestPrice:
+        bike.price.length > 0
+          ? (bike.price[0].price as Decimal).toNumber()
+          : null,
+    };
   }
 }

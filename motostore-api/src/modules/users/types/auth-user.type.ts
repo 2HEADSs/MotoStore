1b9f881f641f1b9d49d8cf92edf66f1598f1dd63
Pavel.Dimitrov <2heads.pd@gmail.com>
2025-09-21 22:40:54 +0300

    create auth-user type for authorization

---
+++ motostore-api/src/modules/users/types/auth-user.type.ts
@@ -0,0 +1,11 @@
+export type AuthUser = {
+  id: string;
+  email: string;
+  username: string;
+  phone: string;
+  role: string;
+  isBlocked: boolean;
+  createdAt: Date;
+  updatedAt: Date;
+  hashedPassword: string;
+};

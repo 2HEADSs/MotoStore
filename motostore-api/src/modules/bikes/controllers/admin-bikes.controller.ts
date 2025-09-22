763d1177c00eb87dd1e7fd9a863af3740af33d90
Pavel.Dimitrov <2heads.pd@gmail.com>
2025-09-22 15:48:37 +0300

    add ApiOkResponse to admin-bike controller

--- motostore-api/src/modules/bikes/controllers/admin-bikes.controller.ts
+++ motostore-api/src/modules/bikes/controllers/admin-bikes.controller.ts
@@ -10,10 +10,17 @@ import {
 import { AdminBikesService } from '../services/admin-bikes.service';
 import { JwtAuthGuard } from 'src/modules/auth/guards/jwt-auth.guard';
 import { AdminGuard } from 'src/modules/auth/guards/admin.guard';
-import { ApiBearerAuth, ApiBody, ApiOperation, ApiTags } from '@nestjs/swagger';
+import {
+  ApiBearerAuth,
+  ApiBody,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
 import { AdminUpdateBikeDto } from '../dto/adminUpdateBike.dto';
 import { AdminUpdateBikeStatusDto } from '../dto/updateBike.dto';
 import { BikesStatusFilterDto } from '../dto/bikesStatusFilter.dto';
+import { BikeResponseDto } from '../dto/BikeResponseDto.dto';
 
 @ApiTags('AdminBikes')
 @ApiBearerAuth('access-token')
@@ -24,6 +31,7 @@ export class AdminBikesController {
 
   @Get('all')
   @ApiOperation({ summary: 'List all bikes, optionally filtered by status' })
+  @ApiOkResponse({ type: BikeResponseDto, isArray: true })
   getAll(@Query() filter: BikesStatusFilterDto) {
     // console.log(filter.status);
     return this.adminBikesService.findAll(filter.status);
@@ -31,6 +39,7 @@ export class AdminBikesController {
 
   @Patch(':id')
   @ApiOperation({ summary: 'Admin: update any bike listing' })
+  @ApiOkResponse({ type: BikeResponseDto })
   @ApiBody({ type: AdminUpdateBikeDto })
   updateBike(@Param('id') bikeId: string, @Body() dto: AdminUpdateBikeDto) {
     return this.adminBikesService.updateBike(bikeId, dto);
@@ -38,12 +47,14 @@ export class AdminBikesController {
 
   @Get(':id')
   @ApiOperation({ summary: 'Admin: get any bike listing' })
+  @ApiOkResponse({ type: BikeResponseDto })
   getBike(@Param('id') bikeId: string) {
     return this.adminBikesService.getOneBike(bikeId);
   }
 
   @Patch(':id/status')
   @ApiOperation({ summary: 'Admin: update any bike listing status' })
+  @ApiOkResponse({ type: BikeResponseDto })
   @ApiBody({ type: AdminUpdateBikeStatusDto })
   updateBikeStatus(
     @Param('id') bikeId: string,

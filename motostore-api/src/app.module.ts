3f75afd490803b40eaf2f9b912a4536b0e92b5bf
Pavel.Dimitrov <2heads.pd@gmail.com>
2025-09-27 21:02:13 +0300

    install and inject throttler

--- motostore-api/src/app.module.ts
+++ motostore-api/src/app.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './modules/database/database.module';
@@ -6,8 +9,24 @@ import { UsersModule } from './modules/users/users.module';
 import { BikesModule } from './modules/bikes/bikes.module';
 
 @Module({
-  imports: [DatabaseModule, UsersModule, BikesModule],
+  imports: [
+    DatabaseModule,
+    UsersModule,
+    BikesModule,
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60,
+        limit: 10,
+      },
+    ]),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
